c06adb386f0b5057a960e6047be08244e214254f
gaidaasd00 <swiftgaida@list.ru>
2022-09-11 21:46:10 +0300

    1. date work

--- ClockApp/ViewController.swift
+++ ClockApp/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: IBOutlet
+    @IBOutlet weak var timeLable: UILabel!
+    @IBOutlet weak var dateLable: UILabel!
+    
+    private let months:[String] = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+                                            
+                                    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startTime()
     }
-
-
+    
+    
+    private func startTime() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [unowned self] _ in
+            let date = Date()
+            let dataFormatter = DateFormatter()
+            dataFormatter.dateFormat = "hh:mm:ss a"
+            let currentTime = dataFormatter.string(from: date)
+            
+            //date
+            dataFormatter.dateFormat = "dd/MM/yyyy"
+            let currentDate = dataFormatter.string(from: date)
+         
+            dateLable.text = currentDate
+            timeLable.text = currentTime
+        }
+    }
 }
 
